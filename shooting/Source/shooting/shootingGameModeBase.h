b52cdae7e05cd590fdebd612c4b5f6ca342a6b75
heonwoochoo <cngjsdn9@gmail.com>
2023-02-19 22:30:59 +0900

    project setup

---
+++ shooting/Source/shooting/shootingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "shootingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTING_API AshootingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
