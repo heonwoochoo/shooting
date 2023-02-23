65898e3e3ae9e6fbc24321cf64e1889e5e1c0ea4
heonwoochoo <cngjsdn9@gmail.com>
2023-02-23 21:41:12 +0900

    Set character movement properties

--- shooting/Source/shooting/Private/Character/ShooterAnimInstance.cpp
+++ shooting/Source/shooting/Private/Character/ShooterAnimInstance.cpp
@@ -1,8 +1,52 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Character/ShooterAnimInstance.h"
+#include "Character/ShooterCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UShooterAnimInstance::UpdateAnimationProperties(float DeltaTime)
+{
+	if (ShooterCharacter == nullptr)
+	{
+		ShooterCharacter = Cast<AShooterCharacter>(TryGetPawnOwner());
+	}
+
+	if (ShooterCharacter)
+	{
+		SetCharacterSpeed();
+		SetIsInAir();
+		SetIsAccelerating();
+	}
+}
 
 void UShooterAnimInstance::NativeInitializeAnimation()
 {
+	ShooterCharacter = Cast<AShooterCharacter>(TryGetPawnOwner());
 }
+
+// Is the character accelerating ?
+void UShooterAnimInstance::SetIsAccelerating()
+{
+	if (ShooterCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.f)
+	{
+		bIsAccelerating = true;
+	}
+	else
+	{
+		bIsAccelerating = false;
+	}
+}
+
+// Is the character in the air?
+void UShooterAnimInstance::SetIsInAir()
+{
+	bIsInAir = ShooterCharacter->GetCharacterMovement()->IsFalling();
+}
+
+// Get the lateral speed of the character from velocity
+void UShooterAnimInstance::SetCharacterSpeed()
+{
+	FVector Velocity = ShooterCharacter->GetVelocity();
+	Velocity.Z = 0;
+	Speed = Velocity.Size();
+}
+
+
--- shooting/Source/shooting/Public/Character/ShooterAnimInstance.h
+++ shooting/Source/shooting/Public/Character/ShooterAnimInstance.h
@@ -6,14 +6,35 @@
 #include "Animation/AnimInstance.h"
 #include "ShooterAnimInstance.generated.h"
 
-/**
- * 
- */
+class AShooterCharacter;
+
 UCLASS()
 class SHOOTING_API UShooterAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
 	
 public:
+	UFUNCTION(BlueprintCallable)
+	void UpdateAnimationProperties(float DeltaTime);
 	virtual void NativeInitializeAnimation() override;
+
+private:
+	void SetIsAccelerating();
+	void SetIsInAir();
+	void SetCharacterSpeed();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"));
+	AShooterCharacter* ShooterCharacter;
+	
+	/**  The Speed of the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"));
+	float Speed;
+
+	/**  Whether or not the character is in the air */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"));
+	bool bIsInAir;
+
+	/**  Whether or not the character is moving */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"));
+	bool bIsAccelerating;
 };
