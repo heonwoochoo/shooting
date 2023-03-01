9fd1b7ba3ee3c087b289cccb2a38bdf500f35365
heonwoochoo <cngjsdn9@gmail.com>
2023-03-01 10:17:23 +0900

    Create Item class

---
+++ shooting/Source/shooting/Private/Items/Item.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "Items/Item.h"
+#include "Components/BoxComponent.h"
+
+AItem::AItem()
+{
+ 	PrimaryActorTick.bCanEverTick = true;
+
+	ItemMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("ItemMesh"));
+	SetRootComponent(ItemMesh);
+
+	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionBox"));
+	CollisionBox->SetupAttachment(ItemMesh);
+}
+
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ shooting/Source/shooting/Public/Items/Item.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Item.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class SHOOTING_API AItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AItem();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	/** Skeletal Mesh for the item */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	USkeletalMeshComponent* ItemMesh;
+
+	/** Line trace collides with box to show HUD widgets */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	UBoxComponent* CollisionBox;
+
+public:
+
+};
