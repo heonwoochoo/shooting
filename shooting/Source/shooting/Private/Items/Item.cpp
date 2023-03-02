e517c66e805bc428dee05dfcf46e1b4dc60b899a
heonwoochoo <cngjsdn9@gmail.com>
2023-03-02 15:51:17 +0900

    Swap weapon

--- shooting/Source/shooting/Private/Items/Item.cpp
+++ shooting/Source/shooting/Private/Items/Item.cpp
@@ -117,6 +117,7 @@ void AItem::SetItemProperties(EItemState State)
 		break;
 
 	case EItemState::EIS_Equipped:
+		PickupWidget->SetVisibility(false);
 		// Set mesh properties
 		ItemMesh->SetSimulatePhysics(false);
 		ItemMesh->SetEnableGravity(false);
--- shooting/Source/shooting/Public/Character/ShooterCharacter.h
+++ shooting/Source/shooting/Public/Character/ShooterCharacter.h
@@ -82,6 +82,9 @@ protected:
 
 	void SelectButtonPressed();
 	void SelectButtonReleased();
+	
+	/** Drops currently equipped Weapon and Equips TraceHitItem */
+	void SwapWeapon(AWeapon* WeaponToSwap);
 
 public:	
 	virtual void Tick(float DeltaTime) override;
@@ -224,7 +227,9 @@ private:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Combat, meta = (AllowPrivateAccess = "true"))
 	TSubclassOf<AWeapon> DefaultWeaponClass;
 
-
+	/** The item currently hit by our trace in TraceForItems (could be null)*/
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Combat, meta = (AllowPrivateAccess = "ture"))
+	AItem* TraceHitItem;
 
 
 	void CreateSpringArm();
